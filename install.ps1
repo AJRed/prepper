2ebf5a63072c8a5047a21acad628281eead64ac2
Andrew Ruedisueli <redchief94@gmail.com>
2025-04-17 14:18:58 -0400

    Fixed installer

--- install.ps1
+++ install.ps1
@@ -1,4 +1,27 @@
 #!/usr/bin/pwsh
+#Set-PSDebug -off
+$mode = 1 # Set operation mode (0 = testing, 1 = prod)
 
-cp -r "D:\scripts\powershell\prepper" "C:\"
+$projectRoot = @(
+    "scripts\powershell\prepper",
+    "scripts\powershell\prepper"
+)
+
+$path = $MyInvocation.MyCommand.Path
+$path = $path -split "\\", 2
+$driveLetter = $path[0]
+$exec = @("echo", "cp")
+$args = @(
+    "`"cp -r $driveLetter\scripts\powershell\prepper C:\`"",
+    "-r $driveLetter\scripts\powershell\prepper C:\",
+    "`"Asshole`""
+)
+
+$cmd = [string]::Format('{0} {1}',$exec[$mode], $args[$mode])
+Invoke-Expression -Command $cmd
+
+
+#$opt = $opt -split "\n"
+#echo $cmd
+#echo "cp -r $driveLetter\scripts\powershell\prepper" "C:\"
 
